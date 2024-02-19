4b1c044e4e22f2d995ce74fcb0481a8697120a10
Sidharth PT <sidharthanand73@gmail.com>
2024-02-19 19:08:22 +0530

    Verify google token
    -Getting the user information from the google token.
    -Creating new user if user doesnt exists

--- src/app/user/resolvers.ts
+++ src/app/user/resolvers.ts
@@ -1,6 +1,50 @@
+import axios from "axios"
+import { prismaClient } from "../../clients/db"
+
+interface GoogleTokenResult {
+    iss?: string;
+    nbf?: string;
+    aud?: string;
+    sub?: string;
+    email: string;
+    email_verified: string;
+    azp?: string;
+    name?: string;
+    picture?: string;
+    given_name: string;
+    family_name?: string;
+    iat?: string;
+    exp?: string;
+    jti?: string;
+    alg?: string;
+    kid?: string;
+    typ?: string;
+  }
+
 const queries = {
-    verifyGoogleToken: async (parent: any, { token }: { token: String }) => {
-        return token
+    verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
+        const googleToken = token
+        const googleAuthUrl = new URL("https://oauth2.googleapis.com/tokeninfo")
+        googleAuthUrl.searchParams.set("id_token", googleToken)
+        
+        const { data } = await axios.get<GoogleTokenResult>(googleAuthUrl.toString(), {
+            responseType:"json"
+        })
+
+        const user = await prismaClient.user.findUnique({
+            where:{email:data.email}
+        })
+
+        if (!user) {
+            await prismaClient.user.create({
+                data: {
+                    email: data.email,
+                    firstName: data.given_name,
+                    lastName: data.family_name,
+                    profileImage:data.picture
+                }
+            })
+        }
     }
 }
 
